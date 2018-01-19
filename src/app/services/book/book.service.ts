a072f8378a5aaecf16a82d8a3566623dcbe61f8e
PauMB <paumb85@gmail.com>
2018-01-19 21:57:58 +0100

    change url

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
 export class BookService extends DataService {
 
   constructor(http: HttpClient) {
-    super('myUrl', http);
+    super('myUrlFrom', http);
   }
 
 }
